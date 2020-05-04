89f12c54c27fdb3c7d7fa588cd6bfe68c7bcb792
nohyoungjin <45346737+nohyoungjin@users.noreply.github.com>
2020-05-04 10:08:16 +0900

    include

---
+++ inc/snb.js
@@ -0,0 +1,19 @@
+str = ''																				
+str += '<div class="snb">'
+str += '	<div class="snb_list">'
+str += '		<h2 class="snb_tit"><span>요양원소개</span></h2>'
+str += '		<ul class="snb_nav">'
+str += '			<li class="on"><a href="greeting.html">인사말</a></li>'
+str += '			<li><a href="intro.html">법인소개</a></li>'
+str += '			<li><a href="mission.html">비전</a></li>'
+str += '			<li><a href="orga.html">조직도</a></li>'
+str += '			<li><a href="staff.html">직원소개</a></li>'
+str += '			<li><a href="history.html">연혁</a></li>'
+str += '			<li><a href="map.html">오시는길</a></li>'
+str += '		</ul>'
+str += '	</div>'
+str += '</div>'
+																						
+document.write( str );						
+
+
