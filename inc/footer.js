d989c9a8dd955352364a3c2bee384637fd2fcede
nohyoungjin <45346737+nohyoungjin@users.noreply.github.com>
2020-09-06 13:42:47 +0900

    Update 2

--- inc/footer.js
+++ inc/footer.js
@@ -1,5 +1,5 @@
 str = ''
-str += '<div class="footer">'
+str += '<div id="footer">'
 str += '	<div class="f_wrap">'
 str += '		<div class="f_holder">'
 str += '			<p class="txt">FAMILY SITE</a></p>'
